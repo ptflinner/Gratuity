2f5917afb818a898f54fdd4161a266e1bde18a09
ptflinner <pflinne@calstatela.edu>
2017-06-12 17:35:09 -0700

    Initial commit

---
+++ app/src/main/java/com/example/patrick/gratuity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.patrick.gratuity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
